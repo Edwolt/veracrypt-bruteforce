0762b46b172375d9a9bb79a23d3b68159b83aa1c
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2024-03-02 17:59:48 -0300

    More Refactor and add a new exhaustive guess function
    Adding a new exhaustive guess function for when you know the size of the
    password

--- bruteforce.py
+++ bruteforce.py
@@ -5,13 +5,18 @@ import argparse
 import itertools
 
 
+ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-def gen_guess_alpha_numeric(size=1):
-    CHARS = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+def gen_guess_alpha_numeric(size=1, alphabet=ALPHABET):
     yield ""
     for i in range(1, size + 1):
-        combinations = itertools.product(CHARS, repeat=i)
-        yield from ("".join(comb) for comb in combinations)
+        yield from gen_guess_exact_alpha_numeric(i, alphabet)
+
+
+def gen_guess_exact_alpha_numeric(size=1, alphabet=ALPHABET):
+    combinations = itertools.product(alphabet, repeat=size)
+    yield from ("".join(comb) for comb in combinations)
 
 
 def gen_guess_from_file(guesses_path):
@@ -22,11 +27,7 @@ def gen_guess_from_file(guesses_path):
 
 
 def load_status(status_file_path, volume_path):
-    status = {
-        "success": set(),
-        "failure": set(),
-        "volume_path": volume_path,
-    }
+    status = {"success": set(), "failure": set(), "volume_path": volume_path}
     try:
         with open(status_file_path, "rb") as status_file:
             status = pickle.load(status_file)
@@ -43,16 +44,47 @@ def load_status(status_file_path, volume_path):
         exit(1)
 
     if status["success"]:
-        print("successful passwords already known: {}".format(status["success"]))
+        print(
+            "successful passwords already known: {}".format(status["success"])
+        )
         print("use a different status file to search again")
         exit(0)
 
     return status
 
 
-def status_dump(status_file_path, status):
+def dump_status(status_file_path, status):
     with open(status_file_path, "wb") as status_file:
         pickle.dump(status, status_file)
+
+
+def make_guess(volume_path, mount_path, status, guess):
+    try:
+        print("trying '{}'".format(guess))
+        # TODO: call is not working
+        subprocess.check_call(
+            [
+                "veracrypt",
+                "--text",  # Uses the text interface
+                "--password={}".format(guess),
+                "--protect-hidden=no",
+                "--keyfiles=",
+                # Prevents re-prompting for passphrase on failure.
+                "--non-interactive",
+                # 485 is the default for most devices.
+                # "--pim=485",
+                volume_path,
+                mount_path,
+            ],
+            # Timeout allows faster guesses but may risk false negatives.
+            # Added based on noticing that a failed password
+            # takes much longer than a successful one.
+            # timeout=2
+        )
+        print("'{}' worked".format(guess))
+        status["success"].add(guess)
+    except (subprocess.CalledProcessError, subprocess.TimeoutExpired):
+        status["failure"].add(guess)
 
 
 def check_guesses(volume_path, mount_path, status_file_path, guesses):
@@ -63,33 +95,8 @@ def check_guesses(volume_path, mount_path, status_file_path, guesses):
             print("skipping '{}' - already attempted".format(guess))
             continue
 
-        try:
-            print("trying '{}'".format(guess))
-            subprocess.check_call(
-                [
-                    "veracrypt",
-                    "--text",  # Uses the text interface
-                    "--password={}".format(guess),
-                    "--protect-hidden=no",
-                    "--keyfiles=",
-                    # Prevents re-prompting for passphrase on failure.
-                    "--non-interactive",
-                    # 485 is the default for most devices.
-                    # "--pim=485",
-                    volume_path,
-                    mount_path,
-                ],
-                # Timeout allows faster guesses but may risk false negatives.
-                # Added based on noticing that a failed password
-                # takes much longer than a successful one.
-                # timeout=2
-            )
-            print("'{}' worked".format(guess))
-            status["success"].add(guess)
-        except (subprocess.CalledProcessError, subprocess.TimeoutExpired):
-            status["failure"].add(guess)
-
-        status_dump(status_file_path, status)
+        make_guess(volume_path, mount_path, status, guess)
+        dump_status(status_file_path, status)
 
         if status["success"]:
             return
